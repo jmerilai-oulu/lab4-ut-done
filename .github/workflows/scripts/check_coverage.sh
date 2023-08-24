8dd119532d6dd63f9879d67cc05aeed92cebd369
jmerilai-oulu <jmerilai19@univ.yo.oulu.fi>
2023-08-24 19:45:49 +0300

    grep

--- .github/workflows/scripts/check_coverage.sh
+++ .github/workflows/scripts/check_coverage.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Run cargo tarpaulin and capture the coverage percentage
 coverage_output=$(cargo tarpaulin)
 
-# Extract the coverage percentage using grep and awk
-coverage_percentage=$(echo "$coverage_output" | grep -oP '100\.00%' | awk '{print $1}')
+coverage_percentage=$(echo "$coverage_output" | grep -oP '(\d+\.\d+)%' | awk '{print $1}')
 
-# Convert coverage percentage to a floating-point number for comparison
 coverage_float=$(echo "$coverage_percentage" | sed 's/%//')
 
-# Check if coverage percentage is greater than or equal to 90
-if (( $(echo "$coverage_float >= 90" | bc -l) )); then
+if (( $(echo "$coverage_float >= 95" | bc -l) )); then
   echo "Coverage is over 90%: $coverage_percentage"
 else
   echo "Coverage is below 90%: $coverage_percentage"
--- .github/workflows/scripts/run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Run tests and generate coverage report
-cargo tarpaulin --out Xml
-bash <(curl -s https://codecov.io/bash) -f coverage.xml -t "$CODECOV_TOKEN" || echo "Codecov failed to upload"
